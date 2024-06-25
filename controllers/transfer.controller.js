85dd6bbcd144f9e47cbdb115104cbe9cece04504
shreykosti <kostishrey135@gmail.com>
2024-06-26 01:07:24 +0530

    zod validation of transfer page

--- controllers/transfer.controller.js
+++ controllers/transfer.controller.js
@@ -1,11 +1,21 @@
 import Account from "../model/account.model.js";
 import mongoose from "mongoose";
+import zod from "zod";
 const App = async (req, res) => {
+  const balanceSchema = zod.number().positive();
+  const toSchema = zod.string();
   const session = await mongoose.startSession();
 
   session.startTransaction();
   const { amount, to } = req.body;
-
+  const check1 = balanceSchema.safeParse(amount);
+  const check2 = toSchema.safeParse(to);
+  if (check1.success === false || check2.success === false) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Invalid amount",
+    });
+  }
   // Fetch the accounts within the transaction
   const account = await Account.findOne({ userNumber: req.user.id }).session(
     session
