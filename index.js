2267d0e70daef88c07bdb8269d6e39f6410d904f
shreykosti <158189084+shreykosti@users.noreply.github.com>
2024-07-13 00:40:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const mongodburl = process.env.MONGOODBCONNECT;
 })();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://payment-app-backend-8ug9.onrender.com",
     credentials: true,
   })
 );
